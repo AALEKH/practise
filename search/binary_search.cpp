9d53fc5cc89c92f57d0fff9c308247c2eb1d593c
Aalekh Nigam <aalekh.nigam@gmail.com>
2016-08-28 01:27:54 +0530

    Update binary_search.cpp

--- search/binary_search.cpp
+++ search/binary_search.cpp
@@ -1,38 +1,52 @@
-#include <bits/stdc++.h>
+/*
+You are given an array A of size N, and Q queries to deal with. 
+For each query, you are given an integer X, and you're supposed to find out if X is present in the array A or not.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-bool binarySearch(vector<int> A, int left, int right, int item)  {
-    if (left <= right) 
-    {
-        int mid = left + (right - left)/2;
-        if (A[mid] == item)
-            return true;
-        else if (item < A[mid])
-        {
-            return binarySearch(A, left, mid-1, item);        
-        }
-        else
-        {
-             return binarySearch(A, mid+1, right, item);     
-        }
-   }
-   else
-        return false;
+bool binarySearch(vector<int> arr, int left, int right, int element){
+	if(left <= right){
+		int middle = left + (right - left)/2;
+		if(arr[middle] == element){
+			return true;
+		}else{
+			if(arr[middle] > element){
+				return binarySearch(arr, left, middle-1, element);
+			}
+			if(arr[middle] < element){
+				return binarySearch(arr, middle+1, right, element);	
+			}
+		}		
+	}else{
+		return false;	
+	}
 }
 
-int main(){
-  //int arr[5] = {3, 4, 6, 1, 5};
-  vector<int> arr_e;
-  arr_e.push_back(3);
-  arr_e.push_back(4);
-  arr_e.push_back(6);
-  arr_e.push_back(1);
-  arr_e.push_back(0);
-  arr_e.push_back(3);
-  int element = 3;
-  sort(arr_e.begin(), arr_e.end());
-  if(binarySearch(arr_e, 0, arr_e.size()-1, element)){
-    cout << "Element Found" << "\n";
-  }
+int main()
+{
+	int size;
+	int no_elements, element, arr_ele;
+	vector<int> ele;
+	cin >> size;
+	cin >> no_elements;
+	while(size > 0){
+		cin >> arr_ele;
+		ele.push_back(arr_ele);
+		size--;
+	}
+	while(no_elements > 0){
+		cin >> element;
+		if(binarySearch(ele, 0, ele.size()-1, element)){
+			cout << "YES" << "\n";
+		}else{
+			cout << "NO" << "\n";
+		}
+		no_elements--;
+	}
+    return 0;
 }
